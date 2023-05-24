ddf1131f2e7dcde5590bf3dd0674ef516c398644
MINSK\Dzmitry_Holub <dzmitry_holub@epam.com>
2023-05-24 15:34:21 +0200

    [Task-3] Added LocalStorageService

--- src/app/shared/components/fake/fake.component.ts
+++ src/app/shared/components/fake/fake.component.ts
@@ -5,6 +5,7 @@ import { ConfigOptionsService } from '../../services/config-options.service';
 import { IConfig } from '../../models/config';
 import { APP_CONFIG, APP_CONFIG_TOKEN } from '../../services/constant.service';
 import { GenerateFactory, GenerateFactoryToken, GeneratorService } from '../../services/generator.service';
+import { LocalStorageService, LocalStorageServiceToken } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-fake',
@@ -12,6 +13,7 @@ import { GenerateFactory, GenerateFactoryToken, GeneratorService } from '../../s
   styleUrls: ['./fake.component.sass'],
   providers: [
     { provide: APP_CONFIG_TOKEN, useValue: APP_CONFIG },
+    { provide: LocalStorageServiceToken, useValue: new LocalStorageService() },
     { provide: GenerateFactoryToken, useFactory: GenerateFactory(10), deps: [GeneratorService] }
   ]
 })
@@ -32,18 +34,24 @@ export class FakeComponent {
   @Output()
   nextId!: number;
 
+  @Output()
+  localStorageData!: string | null;
+
   constructor(
     config: ConfigOptionsService,
     @Optional() private generatorService: GeneratorService,
+    @Optional() @Inject(LocalStorageServiceToken) localStorageService: LocalStorageService,
     @Optional() @Inject(APP_CONFIG_TOKEN) appConfig: any,
     @Optional() @Inject(GenerateFactoryToken) generatedString: string) {
 
       config?.setConfig({ id: "id", login: "login" });
       config?.setConfigProperty("email", "email@email.com");
+      localStorageService?.saveData("data", "data from local storage");
 
       this.config = config?.getConfig();
       this.appConfig = appConfig;
       this.generatedString = generatedString;
+      this.localStorageData = localStorageService?.getData("data");
   }
 
   onGeneratorClick() {
---
+++ src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, InjectionToken } from '@angular/core';
+
+export const LocalStorageServiceToken = new InjectionToken<LocalStorageService>('LocalStorageService');
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public getData(key: string) {
+    return localStorage.getItem(key)
+  }
+  public removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clearData() {
+    localStorage.clear();
+  }
+
+}
