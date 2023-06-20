262d5d0d688c48bcd6f9ee8cdf705339a8b66a8f
MINSK\Dzmitry_Holub <dzmitry_holub@epam.com>
2023-06-20 11:24:05 +0200

    [Task-5] Added routing and json server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './products';
+import { CartComponent } from './purchases';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    component: ProductListComponent 
+  }, 
+  { 
+    path: 'cart', 
+    component: CartComponent 
+  }, 
+  { 
+    path: '', 
+    redirectTo: '/home', 
+    pathMatch: 'full' 
+  }, 
+  { 
+    // The router will match this route if the URL requested 
+    // doesn't match any paths for routes defined in our configuration 
+    path: '**', 
+    component: ProductListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -2,21 +2,19 @@ import { Injectable } from '@angular/core';
 import { Category } from '../enums/category.enum';
 import { Product, IProduct } from '../models/product';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-constructor() { }
+productsUrl = "http://localhost:3000/products";
+
+constructor(private http: HttpClient,) { }
 
 getProducts() : Observable<Array<IProduct>> {
-  return of([
-    new Product("How to be inactive", "This book is actually about how to be inactive", 150, Category.Book, false),
-    new Product("How to be an active", "Press the button", 150, Category.Book, true),
-    new Product("How to be a short", "Shorter than a book", 150, Category.Notepad, true),
-    new Product("Just a book", "This book is about appreciating yourself", 15000000, Category.Book, true),
-  ]);
+  return this.http.get<Product[]>(this.productsUrl);
 }
 
 }
